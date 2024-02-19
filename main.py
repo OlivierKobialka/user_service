11bd557f3041c3c6099da37eb9483b991f88a0f3
_kvbialka917 <113932706+OlivierK07@users.noreply.github.com>
2024-02-19 13:00:52 +0100

    get and post

--- main.py
+++ main.py
@@ -5,28 +5,36 @@ app = Flask(__name__)
 
 
 class User:
-    def __init__(self, firstName: str, lastName: str, birthYear: int, group: str):
-        self.firstName = firstName
-        self.lastName = lastName
-        self.birthYear = birthYear
+    def __init__(self, first_name: str, last_name: str, birth_year: int, group: str):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.birth_year = birth_year
         self.group = group
 
     def __repr__(self):
-        return f"User(firstName='{self.firstName}', lastName='{self.lastName}', birthYear={self.birthYear}, group='{self.group}')"
+        return f"User(first_name='{self.first_name}', last_name='{self.last_name}', birth_year={self.birth_year}, group='{self.group}')"
 
 
 all_users: List[User] = [
-    User(firstName="John", lastName="Doe", birthYear=1990, group="admin"),
-    User(firstName="Jane", lastName="Doe", birthYear=1991, group="user")
+    User(first_name="John", last_name="Doe", birth_year=1990, group="admin"),
+    User(first_name="Jane", last_name="Doe", birth_year=1991, group="user")
 ]
 
 
 @app.route('/users', methods=['GET', 'POST'])
 def users() -> Union[str, Response]:
     if request.method == 'POST':
-        return "POST"
+        data = request.json
+        new_user: User = User(
+            first_name=data['first_name'],
+            last_name=data['last_name'],
+            birth_year=data['birth_year'],
+            group=data['group']
+        )
+        all_users.append(new_user)
+        return Response(str(new_user), status=201)
     elif request.method == 'GET':
-        pass
+        return Response(str(all_users), status=200)
     else:
         return Response("Method not allowed", status=405)
 
@@ -38,6 +46,6 @@ def by_user_id(id: int) -> Response:
     elif request.method == 'DELETE':
         pass
     elif request.method == 'GET':
-        pass
+        return Response(str(all_users[id]), status=200)
     else:
         return Response("Method not allowed", status=405)
